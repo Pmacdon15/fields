ef55d24ba4c9d005c53c4ff2324e5bc832c7f01c
Patrick MacDonald <patrick@patmac.ca>
2025-10-22 20:45:34 -0600

    first commit

---
+++ src/componenets/ui/form/form-input.tsx
@@ -0,0 +1,21 @@
+import { Input } from "../ui/input";
+import { FormBase, type FormControlProps } from "./FormBase";
+
+
+export function FormInput(props: FormControlProps) {
+  const field = useFieldContext<string>();
+  const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
+
+  return (
+    <FormBase {...props}>
+      <Input
+        id={field.name}
+        name={field.name}
+        value={field.state.value}
+        onBlur={field.handleBlur}
+        onChange={(e) => field.handleChange(e.target.value)}
+        aria-invalid={isInvalid}
+      />
+    </FormBase>
+  );
+}
